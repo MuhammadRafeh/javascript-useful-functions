80b7089e3ac7a3f4bb67560e10ad62b7d31eeb6a
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-01 23:59:19 +0500

    Improved TypeScript!

--- src/functions.tsx
+++ src/functions.tsx
@@ -1,1 +1,1 @@
-export const objToArrOfObj = (object: object): Array<any> => Object.keys(object).map(key => ({ [key]: object[key] }))
+export const objToArrOfObj = (object: object): Array<object> => Object.keys(object).map(key => ({ [key]: object[key] }))
